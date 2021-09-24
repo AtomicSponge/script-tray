16e188b7e94aca57c0b8aefc8be9d853dfc4b911
Matthew Evans <mevans+git@wtfsystems.net>
2021-09-24 15:40:54 -0400

    sync

--- assets/preload.js
+++ assets/preload.js
@@ -1,8 +1,10 @@
-const editor = new JSONEditor(document.getElementById("jsonEditor"), {})
+document.onload = () => {
+    const editor = new JSONEditor(document.getElementById("jsonEditor"), {})
 
-require('electron').ipcRenderer.on('send-json-data', (event, message) => {
-    editor.set(JSON.parse(message))
-})
+    require('electron').ipcRenderer.on('send-json-data', (event, message) => {
+        editor.set(JSON.parse(message))
+    })
+}
 
 /*document.querySelector('#donebtn').addEventListener('click', () => {
     require('electron').ipcRenderer.invoke('recieve-json-data', (event, message) => {
