b077635347ecd2b8044abac4846ca51755cab7b1
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-11 22:12:50 -0400

    sync

--- vite.input.config.ts
+++ vite.input.config.ts
@@ -1,13 +1,17 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import renderer from 'vite-plugin-electron-renderer'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [ vue() ],
+  plugins: [ renderer(), vue() ],
   build: {
     outDir: 'dist',
     emptyOutDir: false,
     minify: false,
     rollupOptions: { input: path.join(__dirname, 'html/input.html') }
+  },
+  esbuild: {
+    target: 'esnext'
   }
 })
