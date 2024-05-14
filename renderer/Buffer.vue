f656eb2b8419b97a03c295d50a30efbbebc9f241
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-13 21:50:50 -0400

    sync

--- renderer/Buffer.vue
+++ renderer/Buffer.vue
@@ -5,17 +5,38 @@
 -->
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const _buffer = ref()
+
+/**
+ * Format text to HTML
+ * @param text Unformatted text
+ * @returns Formatted text
+ */
+const formatText = (text:string):string => {
+  text = text.replace(/(?:\r\n|\r|\n)/g, '<br>')
+  return text
+}
+
 window.onload = () => {
   window.bufferAPI.onUpdateBuffer((bufferData:string) => {
-    const bufferOutput = <HTMLDivElement>document.getElementById('bufferOutput')
-    bufferOutput.innerText = bufferData
+    _buffer.value = formatText(bufferData)
   })
 }
 </script>
 
 <template>
-  <div id="bufferOutput"></div>
+  <div class="buffer-view">
+    <span v-html="_buffer"></span>
+  </div>
 </template>
 
 <style lang="stylus" scoped>
+.buffer-view
+  font-size smaller
+  padding-top 2px
+  padding-left 4px
+  padding-right 4px
+  padding-bottom 2px
 </style>
