59c2745d699fe215fa300714b804a18c81d576f6
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-26 11:07:12 -0400

    sync

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -87,18 +87,11 @@ declare global {
     argument:string
   }
 
-  /** Object for argument display label and replacement variable */
-  interface Argument {
-    label:string
-    variable:string
-  }
-
   /** Command object used in menu building */
   interface TrayCommand {
     label:string
     command:string
     cwd:string
-    args:Array<Argument>
   }
 
   /** Sub menu object used in menu building */
--- renderer/components/TrayCommand.vue
+++ renderer/components/TrayCommand.vue
@@ -9,19 +9,6 @@ import type { ModelRef } from 'vue'
 
 /** Model for the tray command reference  */
 const _trayCommand:ModelRef<TrayCommand> = defineModel({ required: true })
-
-/** Add a new argument */
-const addArgument = () => {
-  _trayCommand.value.args.push({ label: 'new arg', variable: 'variable_name' })
-}
-
-/**
- * Remove an argument by index
- * @param idx Index of argument to remove
- */
-const deleteArgument = (idx:number) => {
-  _trayCommand.value.args.splice(idx, 1)
-}
 </script>
 
 <template>
@@ -39,29 +26,6 @@ const deleteArgument = (idx:number) => {
     <td><label for="cmdCwd">CWD:</label></td>
     <td><input type="text" v-model="_trayCommand.cwd" id="cmdCwd"/></td>
   </tr>
-  <tr>
-    <td class="args">Args <button @click="addArgument">Add</button></td>
-    <td>
-      <ul>
-        <li v-for="(_arg, idx) in _trayCommand.args">
-          <table>
-            <tr>
-              <td><label for="argLabel">Label:</label></td>
-              <td><input type="text" v-model="_trayCommand.args[idx].label" id="argLabel"/></td>
-              <td rowspan="2">
-                <button @click="deleteArgument(idx)">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td><label for="argVariable">Variable:</label></td>
-              <td><input type="text" v-model="_trayCommand.args[idx].variable" id="argVariable"/></td>
-            </tr>
-          </table>
-          <hr v-show="idx < (_trayCommand.args.length - 1)"/>
-        </li>
-      </ul>
-    </td>
-  </tr>
 </table>
 </section>
 </template>
@@ -69,20 +33,12 @@ const deleteArgument = (idx:number) => {
 <style lang="stylus" scoped>
 table
   table-layout fixed
-hr
-  border 1px solid rgb(100, 100, 100, 0.1)
 #cmdLabel
   width 200px
 #cmdCommand
   width 320px
 #cmdCwd
   width 200px
-#argLabel
-  width 180px
-#argVariable
-  width 180px
-.args
-  vertical-align top
 button
   vertical-align top
   font-size 0.6em
