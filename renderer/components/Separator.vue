0c109fe35653ba4243729680c17642402f0c7b35
Matthew Evans <mevans+git@wtfsystems.net>
2025-02-02 16:50:04 -0500

    wip

--- renderer/components/Separator.vue
+++ renderer/components/Separator.vue
@@ -18,9 +18,3 @@ const _separator:ModelRef<Separator> = defineModel({ required: true })
     </v-sheet>
   </v-container>
 </template>
-
-<style scoped>
-.separator {
-  border: 4px solid rgb(100, 100, 100, 0.1);
-}
-</style>
